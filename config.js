0a3112ebf5b1de9ff412d5e5b35d5b442b4f9bad
magnus <dinhbach3062001@gmail.com>
2023-09-09 21:49:14 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+  introTitle: "Minh Anh à!",
+  introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "Miiii giỏi thì ấn vào đây",
+  title: "Thích em nhiều lắm luôn á 😘",
+  desc: "Ai lớp diu 3000000000 ...",
+  btnYes: "Em cũng thích anh hiii <33",
+  btnNo: "Không, Em chỉ coi anh là bạn thôi",
+  question:
+    "Trên thế giới hơn 7 tỉ người mà sao em lại yêu 1 đứa vừa đẹp trai lại giàu như anh ?? Nói ... <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Yêu thì yêu mà không yêu thì hư haizzz",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Tối nay chúc em học bài thiec giỏi nhéeee !",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://www.instagram.com/direct/t/17842710704877179/",
+};
